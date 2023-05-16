a38ce57e7bac35762f68573c36f3cc4ca18a87f8
Andrew1st <andrewrfuerst@gmail.com>
2023-05-16 12:19:34 -0700

    finish ex 1.12

--- part1 - anecdotes/src/App.js
+++ part1 - anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Button = (props) => {
+const NewButton = (props) => {
   return (
     <button onClick={props.newAnecdote}>
       {props.text}
@@ -8,29 +8,77 @@ const Button = (props) => {
   )
 } 
 
+const VoteButton = (props) => {
+  return (
+    <button onClick={props.voteAnecdote}>
+      {props.text}
+    </button>
+  )
+} 
+
+const AnecdoteText = (props) => {
+  return (
+    <p>has {props.numVotes} votes</p>
+  )
+}
+
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    '0If it hurts, do it more often.',
+    '1Adding manpower to a late software project makes it later!',
+    '2The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    '3Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    '4Premature optimization is the root of all evil.',
+    '5Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    '6Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    '7The only way to go fast, is to go well.'
   ]
-   
+  const n = anecdotes.length
+
+  // let initialVotes = Array(n).fill(0)
+  // const [vselected, vsetSelected] = useState(initialVotes)
+  const [allVotes, setAllVotes] = useState([])
+
+  const [votesdisplay, setVotesDisplay] = useState(0)
   const [selected, setSelected] = useState(0)
 
-  const randomIdx = newIdx => {
-    setSelected(Math.floor(Math.random() * newIdx))
+  const randomIdx = rdmIn => {
+    const new_i = Math.floor(Math.random() * rdmIn)
+    setSelected(new_i)
+    console.log("new_i: " + new_i)
+
+    let tmpVotesNum = 0
+    let i = 0
+    for(i=0; i < allVotes.length; i++){
+      if (allVotes[i] === new_i){
+      tmpVotesNum += 1  
+      // console.log("NEW I:" + new_i + " | vote found (index allVotes)- " + i)
+      }
+    }
+    // console.log("final tmpvotesnum: " + tmpVotesNum)
+    setVotesDisplay(tmpVotesNum)
+  }
+
+  const voteAnec = idxIn => {
+    // TODO!! Need to figure out how to handle state of votes array(?)
+    let allVotesCopy = [...allVotes]
+    allVotesCopy.push(idxIn)
+    setAllVotes(
+      allVotesCopy
+    )
+    console.log("allVotesCopy: " + allVotesCopy)
+    setVotesDisplay(votesdisplay + 1)
+
+    // TODO!! initialVotes and/or vselected needs to be set properly...
   }
 
   return (
     <div>
       {anecdotes[selected]}
       <br />
-      <Button newAnecdote={() =>  randomIdx(anecdotes.length)} text="next anecdote" />
+      <AnecdoteText numVotes={votesdisplay}/>
+      <VoteButton voteAnecdote={() => voteAnec(selected)} text="vote"/>
+      <NewButton newAnecdote={() =>  randomIdx(n)} text="next anecdote" />
     </div>
   )
 }
