a24b6744146eff4fae819c339c9b65e38bf7c2e6
Andrew1st <andrewrfuerst@gmail.com>
2023-05-16 18:57:28 -0700

    start part 2 | start ex. 2.6 (Course information step6) | start/init part 2 branch

---
+++ part2/README.md
@@ -0,0 +1,17 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Part 1: [REACT.JS] Learning Material - My Full Stack Open
+* Introduction to React (https://fullstackopen.com/en/part1/introduction_to_react)
+* Javascript (https://fullstackopen.com/en/part1/java_script)
+* Component state, event handlers (https://fullstackopen.com/en/part1/component_state_event_handlers)
+* A more complex stat, debugging React Apps (https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps)
+
+#### Commits to this repo after finishing each exercise within above chapter
---
+++ part2/src/App.js
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+
+const StatisticLine = props => <tr><td>{props.text}</td><td>{props.value}</td></tr>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+  /*USE STATISTICS COMPONENT OUTSIDE OF MAIN APP FOR DISPLAYING: ALL, AVERAGE, & POSITIVE PERCENT*/
+const Statistics = (props) => {
+  
+  if (props.all === 0){
+    return (
+      <div>
+      <h1> Statistics </h1>
+      <p> No Feedback Given </p>
+      </div>
+    )
+  }
+  return ( 
+    <div>
+      <h1> Statistics </h1>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad}/>
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodPressed = goodVal => {
+    console.log("Good Value New:", goodVal)
+    setGood(goodVal)
+  }
+
+  const neutralPressed = neutralVal => {
+    console.log("neutral Value New:", neutralVal)
+    setNeutral(neutralVal)
+  }
+
+  const badPressed = badVal => {
+    console.log("bad Value New:", badVal)
+    setBad(badVal)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => goodPressed(good + 1)} text="good"/>
+      <Button handleClick={() => neutralPressed(neutral + 1)} text="neutral"/>
+      <Button handleClick={() => badPressed(bad + 1)} text="bad"/>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} all={good + bad + neutral} average={((good-bad)/(good + bad + neutral)).toFixed(1)} positive={(good / (good + bad + neutral) * 100).toFixed(1) + "%"} />
+    </div>
+  )
+}
+
+export default App
