079c0960c2c7a73ad07aab3c4e527bc1c21335a6
Andrew1st <andrewrfuerst@gmail.com>
2023-05-18 22:25:28 -0700

    [ex 2.1] W.I.P | commit to continue work...

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,72 +1,43 @@
-import { useState } from 'react'
+const Course = (props) => {
+  console.log(props)
+  const { name, parts } = props
 
-const StatisticLine = props => <tr><td>{props.text}</td><td>{props.value}</td></tr>
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-  /*USE STATISTICS COMPONENT OUTSIDE OF MAIN APP FOR DISPLAYING: ALL, AVERAGE, & POSITIVE PERCENT*/
-const Statistics = (props) => {
-  
-  if (props.all === 0){
-    return (
-      <div>
-      <h1> Statistics </h1>
-      <p> No Feedback Given </p>
-      </div>
-    )
-  }
-  return ( 
+  return (
     <div>
-      <h1> Statistics </h1>
-      <table>
-        <tbody>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="bad" value={props.bad}/>
-        <StatisticLine text="all" value={props.all} />
-        <StatisticLine text="average" value={props.average} />
-        <StatisticLine text="positive" value={props.positive}/>
-        </tbody>
-      </table>
-    </div>
+      <h1>{name}</h1>
+      <div>
+        {parts.map(part =>
+          <p key={part.id}>{part.name}</p>
+        )}
+      </div>
+    </div>   
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const goodPressed = goodVal => {
-    console.log("Good Value New:", goodVal)
-    setGood(goodVal)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
-
-  const neutralPressed = neutralVal => {
-    console.log("neutral Value New:", neutralVal)
-    setNeutral(neutralVal)
-  }
-
-  const badPressed = badVal => {
-    console.log("bad Value New:", badVal)
-    setBad(badVal)
-  }
-
-  return (
-    <div>
-      <h1>Give Feedback</h1>
-      <Button handleClick={() => goodPressed(good + 1)} text="good"/>
-      <Button handleClick={() => neutralPressed(neutral + 1)} text="neutral"/>
-      <Button handleClick={() => badPressed(bad + 1)} text="bad"/>
-      
-      <Statistics good={good} neutral={neutral} bad={bad} all={good + bad + neutral} average={((good-bad)/(good + bad + neutral)).toFixed(1)} positive={(good / (good + bad + neutral) * 100).toFixed(1) + "%"} />
-    </div>
-  )
+  
+  return <Course course={course} />
 }
 
 export default App
