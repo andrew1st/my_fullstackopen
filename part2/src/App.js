5163a33f22fd468a8c788903fe293472a9351254
Andrew1st <andrewrfuerst@gmail.com>
2023-11-08 23:10:16 -0800

    finish ex. 2.10 | clean project file structure

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,77 +1,76 @@
 import { useState } from 'react'
 
+import Filter from "./components/Filter"
+import Persons from "./components/Person"
+import PersonForm from "./components/PersonForm" 
+
 const App = () => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    console.log("addPerson:")
-    let addPersonBool = true
-    persons.forEach(p => {
-      if (JSON.stringify(p.name) == JSON.stringify(newName)){
-        addPersonBool = false
-      }
-    });
-    if (addPersonBool){
-      const personObject = {
-        name: newName,
-        number: newNumber,
-        id: addPerson.name
-      }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
-    }
-    else {
-      const alert_str = `${newName} is already added to the PhoneBook.`
-      window.alert(alert_str)
-    }
-  }
-  
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-
-  const handlePersonChange = (event) => {
-    console.log("handlePersonChange")
-    console.log(event.target.value)
-    setNewName(event.target.value)
+  const handleFilterChange = (event) => {
+    console.log("handleFilterChange" + event.target.value)
+    setNewFilter(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log("handleNumberChange")
-    console.log(event.target.value)
+    console.log("handleNumberChange" + event.target.value)
     setNewNumber(event.target.value)
+}
+
+  const handlePersonChange = (event) => {
+      console.log("handlePersonChange" + event.target.value)
+      setNewName(event.target.value)
   }
 
-  const handleFilterChange = (event) => {
-    console.log("handleFilterChange")
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
-  }
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    console.log("handleAddPerson:")
+
+    let addPersonBool = true
+    persons.forEach(p => {
+        if (JSON.stringify(p.name) == JSON.stringify(newName)){
+            addPersonBool = false
+          }
+        });
+        if (addPersonBool){
+            const personObject = {
+              name: newName,
+              number: newNumber,
+              id: newName
+            }
+            setPersons(persons.concat(personObject))
+            setNewName('')
+            setNewNumber('')
+        }
+        else {
+          const alert_str = `${newName} is already added to the PhoneBook.`
+          window.alert(alert_str)
+        }
+    }
+  
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>filter shown with <input value={newFilter} onChange={handleFilterChange}/> </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>name: <input value={newName} onChange={handlePersonChange}/></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {personsToShow?.map(person =>
-          <p key={person.name}>{person.name} {person.number}</p>  
-        )}
-      </div>
+
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+
+      <h3>Add a new</h3>
+
+      <PersonForm newName={newName} newNumber={newNumber} handleNumberChange={handleNumberChange} handlePersonChange={handlePersonChange} handleAddPerson={handleAddPerson}/>
+
+      <h3>Numbers</h3>
+
+      <Persons newFilter={newFilter} persons={persons}/>
     </div>
   )
 }
