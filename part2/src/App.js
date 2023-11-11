3258b8e815acf977d5fc2614b21a451512735b81
Andrew1st <andrewrfuerst@gmail.com>
2023-11-10 17:35:14 -0800

    finish ex. 2.11 | axios get w/ db.json

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 import Filter from "./components/Filter"
 import Persons from "./components/Person"
@@ -9,12 +11,17 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
+  // load data from db.json file
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleFilterChange = (event) => {
     console.log("handleFilterChange" + event.target.value)
@@ -37,7 +44,7 @@ const App = () => {
 
     let addPersonBool = true
     persons.forEach(p => {
-        if (JSON.stringify(p.name) == JSON.stringify(newName)){
+        if (JSON.stringify(p.name) === JSON.stringify(newName)){
             addPersonBool = false
           }
         });
